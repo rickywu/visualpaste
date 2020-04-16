2ae6f785c6db912e66e89215d0a1be969d05fedb
Ricky <rickywu1113@gmail.com>
2020-04-16 10:13:57 +0800

    init push

---
+++ VSTOWordProject/CangjieRibbon.Designer.cs
@@ -0,0 +1,117 @@
+﻿namespace VisualPaste
+{
+    partial class CangjieRibbon : Microsoft.Office.Tools.Ribbon.RibbonBase
+    {
+        /// <summary>
+        /// 必需的设计器变量。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        public CangjieRibbon()
+            : base(Globals.Factory.GetRibbonFactory())
+        {
+            InitializeComponent();
+        }
+
+        /// <summary> 
+        /// 清理所有正在使用的资源。
+        /// </summary>
+        /// <param name="disposing">如果应释放托管资源，为 true；否则为 false。</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region 组件设计器生成的代码
+
+        /// <summary>
+        /// 设计器支持所需的方法 - 不要
+        /// 使用代码编辑器修改此方法的内容。
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.TabHome = this.Factory.CreateRibbonTab();
+            this.grpCangjieImport = this.Factory.CreateRibbonGroup();
+            this.btnCangjieImport = this.Factory.CreateRibbonMenu();
+            this.btnTableTitle = this.Factory.CreateRibbonButton();
+            this.btnCopyImp = this.Factory.CreateRibbonButton();
+            this.TabHome.SuspendLayout();
+            this.grpCangjieImport.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // TabHome
+            // 
+            this.TabHome.ControlId.ControlIdType = Microsoft.Office.Tools.Ribbon.RibbonControlIdType.Office;
+            this.TabHome.ControlId.OfficeId = "TabHome";
+            this.TabHome.Groups.Add(this.grpCangjieImport);
+            this.TabHome.Label = "TabHome";
+            this.TabHome.Name = "TabHome";
+            // 
+            // grpCangjieImport
+            // 
+            this.grpCangjieImport.Items.Add(this.btnCangjieImport);
+            this.grpCangjieImport.Name = "grpCangjieImport";
+            // 
+            // btnCangjieImport
+            // 
+            this.btnCangjieImport.ControlSize = Microsoft.Office.Core.RibbonControlSize.RibbonControlSizeLarge;
+            this.btnCangjieImport.Image = global::VSTOWordProject.Properties.Resources.logo;
+            this.btnCangjieImport.Items.Add(this.btnTableTitle);
+            this.btnCangjieImport.Items.Add(this.btnCopyImp);
+            this.btnCangjieImport.ItemSize = Microsoft.Office.Core.RibbonControlSize.RibbonControlSizeLarge;
+            this.btnCangjieImport.Label = "ImpForWeb";
+            this.btnCangjieImport.Name = "btnCangjieImport";
+            this.btnCangjieImport.OfficeImageId = "PageMenu";
+            this.btnCangjieImport.ShowImage = true;
+            // 
+            // btnTableTitle
+            // 
+            this.btnTableTitle.ControlSize = Microsoft.Office.Core.RibbonControlSize.RibbonControlSizeLarge;
+            this.btnTableTitle.Image = global::VSTOWordProject.Properties.Resources.sheet;
+            this.btnTableTitle.Label = "TableHeader";
+            this.btnTableTitle.Name = "btnTableTitle";
+            this.btnTableTitle.ShowImage = true;
+            // 
+            // btnCopyImp
+            // 
+            this.btnCopyImp.ControlSize = Microsoft.Office.Core.RibbonControlSize.RibbonControlSizeLarge;
+            this.btnCopyImp.Image = global::VSTOWordProject.Properties.Resources.copy;
+            this.btnCopyImp.Label = "ExportHTML";
+            this.btnCopyImp.Name = "btnCopyImp";
+            this.btnCopyImp.ShowImage = true;
+            // 
+            // CangjieRibbon
+            // 
+            this.Name = "CangjieRibbon";
+            this.RibbonType = "Microsoft.Word.Document";
+            this.Tabs.Add(this.TabHome);
+            this.Load += new Microsoft.Office.Tools.Ribbon.RibbonUIEventHandler(this.CangjieImportRibbon_Load);
+            this.TabHome.ResumeLayout(false);
+            this.TabHome.PerformLayout();
+            this.grpCangjieImport.ResumeLayout(false);
+            this.grpCangjieImport.PerformLayout();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        internal Microsoft.Office.Tools.Ribbon.RibbonTab TabHome;
+        internal Microsoft.Office.Tools.Ribbon.RibbonGroup grpCangjieImport;
+        internal Microsoft.Office.Tools.Ribbon.RibbonMenu btnCangjieImport;
+        internal Microsoft.Office.Tools.Ribbon.RibbonButton btnTableTitle;
+        internal Microsoft.Office.Tools.Ribbon.RibbonButton btnCopyImp;
+    }
+
+    partial class ThisRibbonCollection
+    {
+        internal CangjieRibbon CangjieRibbon
+        {
+            get { return this.GetRibbon<CangjieRibbon>(); }
+        }
+    }
+}
---
+++ VSTOWordProject/CangjieRibbon.cs
@@ -0,0 +1,98 @@
+﻿using System;
+using Microsoft.Office.Tools.Ribbon;
+using System.Text.RegularExpressions;
+using System.Text;
+using System.IO;
+using Microsoft.Office.Interop.Word;
+
+namespace VisualPaste
+{
+    public partial class CangjieRibbon
+    {
+        private void CangjieImportRibbon_Load(object sender, RibbonUIEventArgs e)
+        {
+            // 为按钮注册点击事件
+            btnTableTitle.Click += new RibbonControlEventHandler(CheckDocument);
+            btnCopyImp.Click += new RibbonControlEventHandler(CheckDocument);
+        }
+
+        // 导出文件方法
+        private void CheckDocument(object sender, RibbonControlEventArgs e)
+        {
+            Document Doc = Globals.ThisAddIn.Application.ActiveDocument;
+            switch (e.Control.Id)
+            {
+                // 判断点击的按钮ID
+                case "btnTableTitle":
+                    foreach (Table tbl in Doc.Tables)
+                    {
+                        tbl.ApplyStyleHeadingRows = true;   
+                    }
+                    break;
+                case "btnCopyImp":
+                    string docName = Doc.FullName;
+                    //select all and copy
+                    //Doc.Application.ActiveDocument.Content.Select();
+                    //Doc.Application.Selection.Copy();
+                    string fileName = Path.Combine(Path.GetTempPath(), "CangJieSaveAs.html");
+                    object missing = System.Reflection.Missing.Value;
+                    object FileName = fileName;
+                    object FileFormat = WdSaveFormat.wdFormatHTML;
+                    Doc.WebOptions.Encoding = Microsoft.Office.Core.MsoEncoding.msoEncodingUTF8;
+                    Doc.Application.ActiveDocument.SaveAs2(ref FileName, ref FileFormat);
+                    Doc.Application.ActiveDocument.Close();
+                    Globals.ThisAddIn.Application.Documents.Open(docName);
+
+                    //load to process
+                    var saveDoc = new HtmlAgilityPack.HtmlDocument();
+                    saveDoc.OptionDefaultStreamEncoding = Encoding.UTF8;
+                    saveDoc.DetectEncodingAndLoad(fileName, true);
+                    //remove comment node
+                    var commentNodes = saveDoc.DocumentNode.SelectNodes("//comment()");
+                    if (commentNodes != null)
+                    {
+                        foreach (var comment in commentNodes)
+                        {
+                            if (!comment.InnerText.StartsWith("<!DOCTYPE"))
+                            {
+                                comment.Remove();
+                            }
+                        }
+                    }
+                    //parse img source
+                    var imgNodes = saveDoc.DocumentNode.SelectNodes("//img/@src");
+                    if (imgNodes != null)
+                    {
+                        foreach (var img in imgNodes)
+                        {
+                            string imageName = img.GetAttributeValue("src", "");
+                            string localPath = Path.Combine(Path.GetTempPath(), imageName);
+                            string imgBase64String = GetBase64StringForImage(localPath);
+                            string imageType = imageName.Substring(imageName.Length - 3);
+                            //use correct MIME type for jpeg image
+                            imageType = (imageType == "jpg") ? imageType.Replace("jpg","jpeg") : imageType;
+                            img.SetAttributeValue("src", "data:image/" + imageType + ";base64," + imgBase64String);
+                        }
+                    }
+                    //clear html
+                    String bodyHtml = saveDoc.DocumentNode.SelectSingleNode("//body").InnerHtml;
+                    bodyHtml = Regex.Replace(bodyHtml, @"( |\t|\r?\n)\1+", "$1");
+                    bodyHtml = bodyHtml.Trim(Environment.NewLine.ToCharArray());
+
+                    //copy to clipboard and delete temp files
+                    Utilities.CopyHtmlToClipBoard(bodyHtml);
+                    File.Delete(fileName);
+                    Directory.Delete(fileName.Replace("html","files"), true);
+                    break;
+                default:
+                    return;
+            }
+        }
+        protected static string GetBase64StringForImage(string imgPath)
+        {
+            byte[] imageBytes = System.IO.File.ReadAllBytes(imgPath);
+            string base64String = Convert.ToBase64String(imageBytes);
+            return base64String;
+        }
+    }
+}
